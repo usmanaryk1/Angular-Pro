5d5b2b3b373eb3f006cb40ffea44f3f02c6d25c7
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-04-11 13:03:51 +0500

    Destroying dynamic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, AfterViewInit, Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ComponentFactoryResolver, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { AuthFormComponent } from './auth-form/auth-form.component';
 @Component({
@@ -17,6 +17,10 @@ import { AuthFormComponent } from './auth-form/auth-form.component';
 <!-- this div act as container -->
 <!-- using this templateRef #entry we are going to use viewChild decarator and communicate directly to this piece of dom to allow us to injuct this component -->
 
+<button (click)="destoryComponent()">
+Destory
+</button>
+
 <div #entry > </div>
 
 </div>
@@ -25,6 +29,9 @@ import { AuthFormComponent } from './auth-form/auth-form.component';
 })
 export class AppComponent implements AfterContentInit ,AfterViewInit {//for use ViewChild implements AfterContentInit lifecycle hook rather then AfterViewInit because we can setup our component subscribe to the output and change the data before the actual view has been initialize 
   title = 'AngularPro';
+
+  //To destroy the dynamic component need to declear globally so that it could access anywhere 
+  component!:ComponentRef<AuthFormComponent>//use the definite assignment assertion operator, !:// Not initialized, but no error
 
   // to interact dom element #entry div use ViewChild
   // passing 2nd argument(read) in ViewChild, this is essentially changes when we get back which its gives us different lockup token that we previously used to viewchild where we got element ref (entry!=> local use in ts)
@@ -45,16 +52,16 @@ export class AppComponent implements AfterContentInit ,AfterViewInit {//for use 
     
     //undefined initialize so use setTimeout not good i dont get idea why its giving error this.entry red underline (gives error only in development mode i think , in production mode error will not accour)
     setTimeout(() => {
-      const component = this.entry.createComponent(authFormFactory)//or const component = this.entry.createComponent(this.resolver.resolveComponentFactory(AuthFormComponent))
+      this.component = this.entry.createComponent(authFormFactory)//or const component = this.entry.createComponent(this.resolver.resolveComponentFactory(AuthFormComponent))
     
     //input=> dynamic component input (not have @Input title=Login but optionally can use) its value can access from its instance (instance exposes all property)(and then overwrite it) which create with createComponent(componentName)
     //this is how dynamically change the @Input() title in compile time
-    console.log(component.instance); //here title public porperty can overwrite//AuthFormComponent {title: 'Login', submitted: EventEmitter_, __ngContext__: LRootView(31)}
-    component.instance.title='Create account';//overwriting(Re-itterate) the title //here in authform title not declear as @Input() title but its value is change with instance but optionally could use @Input() title if like too;
+    console.log(this.component.instance); //here title public porperty can overwrite//AuthFormComponent {title: 'Login', submitted: EventEmitter_, __ngContext__: LRootView(31)}
+    this.component.instance.title='Create account';//overwriting(Re-itterate) the title //here in authform title not declear as @Input() title but its value is change with instance but optionally could use @Input() title if like too;
     
     //output=> dynamic component output can access by subscribing the output event (like submitted output event here)
-    console.log(component.instance.submitted); //here submitted output event can access //AuthFormComponent {title: 'Login', submitted: EventEmitter_, __ngContext__: LRootView(31)}
-    component.instance.submitted.subscribe(this.loginUser);//subscribe the user object here which showing in #entry div;
+    console.log(this.component.instance.submitted); //here submitted output event can access //AuthFormComponent {title: 'Login', submitted: EventEmitter_, __ngContext__: LRootView(31)}
+    this.component.instance.submitted.subscribe(this.loginUser);//subscribe the user object here which showing in #entry div;
   
   
   },1000);
@@ -63,6 +70,12 @@ export class AppComponent implements AfterContentInit ,AfterViewInit {//for use 
 
   ngAfterViewInit(){
   console.log("ngAfterViewInit");
+  }
+
+  //dynamically destory a component which is dynamically created 
+  destoryComponent(){
+    //calling the method and destory the dynamically component
+    this.component.destroy();
   }
 
   loginUser(user:any){
