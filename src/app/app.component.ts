869d18dd6a286eee83fc9cad8f618c990f588bd6
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-04-14 10:38:44 +0500

    Using ngTemplateOutlet with context topassing the context implicit and variable value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,17 @@ import { AuthFormComponent } from './auth-form/auth-form.component';
 <!-- we learned how to injected ng-template into our dom beside to partucular <div #entry ></div> div, using the  API such as creteEmbededView whic is expose to us via a ViewContainerRef -->
 <!-- <div #entry > </div> in this video infact change this div to use this directive called ngTemplateOutlet (using ng-container whichis never get render in the dom ony inside material get render in th dom like div)-->
 
+
 <ng-container
-[ngTemplateOutlet]="tmpl"> <!--this is cleaver directive because we just pass simple template ref tmpl into the ng component outlet and it will be auto maticaly rendered in the dom-->
+[ngTemplateOutlet]="tmpl"
+[ngTemplateOutletContext]="ctx" > <!-- [ngTemplateOutlet]="tmpl" =>this is cleaver directive because we just pass simple template ref tmpl into the ng component outlet and it will be auto maticaly rendered in the dom , and [ngTemplateOutletContext]="ctx" => how to pass context here such as implicit value and variable like in viewContainerRef -->
+
 
 </ng-container>
 
-<ng-template #tmpl > <!-- this ng-template #tmpl will render inside the ng-container with the help of ngTemplateOutlet directive-->
- Tood Motto : England, UK
+<ng-template #tmpl let-name let-location="locations"> <!-- this ng-template #tmpl will render inside the ng-container with the help of ngTemplateOutlet directive-->
+ <!-- Tood Motto : England, UK -->
+ {{name}} : {{ location }}
 </ng-template>
 
 </div>
@@ -25,5 +29,8 @@ import { AuthFormComponent } from './auth-form/auth-form.component';
 })
 export class AppComponent  {
 
-
+ctx={
+  $implicit:'Tood Motto',
+  locations:'England, Uk'
+}
 }
