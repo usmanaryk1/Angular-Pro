2fb681937b73514fd5963128a9303664455551a0
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-06-14 16:39:14 +0500

    Creating a custom pipe like Filesize byte into MB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//custom pipe file object interface
+interface File {
+  name: string,
+  size: number,
+  type: string
+}
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   template:`
-  This is {{title}}.
+  
+  custom Pipes almost like a component directive and gives name:'anyname-which-is-used-in-template with | anyName'  
+  <br><br><br>
+
+  <div>
+
+  
+  <div *ngFor="let file of files">
+    <p>{{ file.name }}</p>
+    <p>{{ file.size | filesize }}</p> <!-- create a custome pipe(like a |date pipe etc..) to transform the size in MB because size is comig in bytes -->
+  </div>
+
+  </div>
+
+  <pre>
+  make our own custome pipes and lets see how does it works
+  </pre>
+
   `
 })
-export class AppComponent {
-  title = 'Custom Pipes start from here';
+export class AppComponent implements OnInit {
+ 
+  files: File[] = [];//initializer =[]
+  ngOnInit() {
+    this.files = [
+      { name: 'logo.svg', size: 2120109, type: 'image/svg' },
+      { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
+      { name: 'background.png', size: 1784562, type: 'image/png' }
+    ];
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//customes pipes
+import { FileSizePipe } from './filesize.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileSizePipe
   ],
   imports: [
     BrowserModule
