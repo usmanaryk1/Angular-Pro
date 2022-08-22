41b04ecdded29f48d4f1d8fc944d26c863391480
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-08-22 15:49:01 +0500

    Pipes as providers means pipe use in js and show that in html after changing for this 1-import it 2-provide it in provider 3-inject it in constructor  4-use in js and show in html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FileSizePipe } from './filesize.pipe';
 
 //custom pipe file object interface
 interface File {
   name: string,
   size: number,
+  type: string
+}
+
+//custom pipe FileMaped object interface
+interface FileMaped {
+  name: string,
+  size: any,//change due to not sure what comes in filter
   type: string
 }
 
@@ -18,29 +26,52 @@ interface File {
   <div>
 
   
-  <div *ngFor="let file of files">
+  <div *ngFor="let file of mapped">
     <p>{{ file.name }}</p>
-    <p>{{ file.size | filesize: ' megabytes' }}</p> <!-- set custome/default arguments 'megabytes' in filter  | filesize: 'megabytes'  watever coming from filter filesize.filter.ts extension 'MB' it will change to magabytes because of spacificity -->
+    <p>{{ file.size }}</p> <!-- set ' mb' filter | filesize: value from JavaScript and then show in html  -->
   </div>
 
   </div>
 
   <pre>
   make our own custome pipes and lets see how does it works.
-  Set custome/default arguments 'megabytes' in filter  | filesize: 'megabytes'  watever coming from filter filesize.filter.ts extension 'MB' it will change to magabytes because of spacificity.
+  Use pipe in javascript and then show in html so in html we don't need to use |filesize pipe.
+  Also provide FileSizePipe in provider if wana use in js.
+  set ' mb' filter | filesize: value from JavaScript and then show in html.
+  1-import the pipe
+  2-register it on the component
+  3-dependency inject it on the particular component
+  4-mapping and transform the data in js inside this component which actually reaches to template where show this piped value which chaged in is already
   </pre>
 
-  `
+  `,
+  //Register this pipe to the component using the providers and dependency inject it inside the constructor means provide FileSizePipe in provider for this component if wana use in js
+  providers:[
+    FileSizePipe
+  ]
 })
 export class AppComponent implements OnInit {
+
+  //dependency inject it inside the constructor
+  constructor(private fileSizePipe:FileSizePipe){}
  
   files: File[] = [];//initializer =[]
+  mapped: FileMaped[] = [];//initializer =[]
   ngOnInit() {
+
     this.files = [
       { name: 'logo.svg', size: 2120109, type: 'image/svg' },
       { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
       { name: 'background.png', size: 1784562, type: 'image/png' }
     ];
+
+    this.mapped= this.files.map( file =>{
+      return {
+        name:file.name,
+        size:this.fileSizePipe.transform(file.size, ' mb'),//if second argument ' mb' not given then in fileSizePipe file default given 'MB' will show 
+        type:file.type
+      }
+    })
   }
 
 }
