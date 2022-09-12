0720dcc2773a81b1a148871283e1b0539534a360
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-09-12 18:53:45 +0500

    Adding keyboard events to our control so keyDown blur and focus event impliment

--- src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-// we are going to using the angular behind the scene contrl of ng-pristine ng-untouched ng-valid etc...
+// we are going to using the angular behind the scene contrl of ng-pristine ng-untouched ng-valid etc... in custome component
 const COUNTER_CONTROL_ACCESSOR = { // create a constant object to provide our component in the providers:[COUNTER_CONTROL_ACCESSOR]
   provide: NG_VALUE_ACCESSOR, // gaining access to our valueAccessor means behind the scene this is talk to the <input type="number"> and control the min max step with formControName means allow NG_VALUE_ACCESSOR by register it on our component
   useExisting: forwardRef(() => StockCounterComponent), //which component will use our access of FormControll//something we need to talk to angular bindings //we set here custome provider which is allow us to registor our stock-counter component 
@@ -18,9 +18,15 @@ const COUNTER_CONTROL_ACCESSOR = { // create a constant object to provide our co
   providers: [COUNTER_CONTROL_ACCESSOR], // implementing COUNTER_CONTROL_ACCESSOR interfave
   styleUrls: ['stock-counter.component.scss'],
   template: `
-    <div class="stock-counter">
+    <div class="stock-counter"
+    [class.focused]="focus">
       <div>
-        <div>
+      <!-- tabindex="0" means on keyboard directally work -->
+        <div 
+          tabindex="0"
+         (keydown)="onKeyDown($event)"
+         (blur)="onBlur($event)"
+         (focus)="onFocus($event)">
           <p>{{ value }}</p>
           <div>
             <!-- disable button when limit reached till max-->
@@ -63,13 +69,49 @@ export class StockCounterComponent implements ControlValueAccessor { //ControlVa
     this.value = value || 0; // assign any initial and changed value 
   }
 
-  //NOTE in next commit ---> In next commit in git we will use keyboard event to tight our blur and focuse events on this component 
-
+  
   @Input() step: number = 10;
   @Input() min: number = 10;
   @Input() max: number = 1000;
-
+  
   value: number = 10; // becaue we select 10 by default in formControl
+  
+  //NOTE ---> we use keyboard event to tight our blur and focuse events on this component 
+  focus!: boolean; // for dynaically border style 
+
+  onKeyDown(event: KeyboardEvent) {
+    console.log("keyboard",event.code,event);
+    
+    const handlers = {
+      ArrowDown: () => this.decrement(),
+      ArrowUp: () => this.increment()
+    };
+
+    if (event.code === 'ArrowUp') {  // check the event weather it is the ArrowUp or ArrowDown 
+      handlers.ArrowUp(); // handlers[event.code]()if write handlers[event.code]() this is not working in typeScript // if event.code will be handlers[ArrowUp] then ArrowUp: () => this.increment() will work or if if event.code will be handlers[ArrowDown] then ArrowDown: () => this.decrement() will work dynamically
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    if (event.code==='ArrowDown') {  // check the event weather it is the ArrowUp or ArrowDown 
+      handlers.ArrowDown(); // if event.code will be handlers[ArrowUp] then ArrowUp: () => this.increment() will work or if if event.code will be handlers[ArrowDown] then ArrowDown: () => this.decrement() will work dynamically
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.onTouch();
+  }
+
+  onBlur(event: FocusEvent) { 
+    this.focus = false; // for dynaically border style 
+    event.preventDefault();
+    event.stopPropagation();
+    this.onTouch();
+  }
+  onFocus(event: FocusEvent) {
+    this.focus = true; // for dynaically border style 
+    event.preventDefault();
+    event.stopPropagation();
+    this.onTouch();
+  }
 
   increment() {
     if (this.value < this.max) { // increment value till max like not more then 1000 
