9196f8ac81dd4f3476ce8ebcc4ec4b68bc0bbd06
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-09-14 14:55:16 +0500

    Custom preloading strategies so preload any module on our choice so make a CustomPreload class and check if data:{preload:true} in any module then preload it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 //http module
 import { HttpClientModule } from '@angular/common/http';
 
-import { RouterModule, Routes , PreloadAllModules } from '@angular/router';
+import { RouterModule, Routes , PreloadingStrategy, Route } from '@angular/router';
 
 import { MailModule } from './mail/mail.module';
 
 import { AppComponent } from './app.component';
+import { Observable, of } from 'rxjs';
+
+//custom preload class 
+export class CustomPreload implements PreloadingStrategy{
+  preload(route:Route, fn:()=> Observable<any>): Observable<any>{//preload comes from implements PreloadingStrategy
+    return route.data && route.data.preload ? fn() : of(null); //check if data objeck available and also have property preload then return a function otherwise return our own observable null
+  }
+} 
 
 export const ROUTES: Routes = [
    //lazzy Loading 
    {
-    path: 'dashboard',
+     path: 'dashboard',
+     //implement CustomPreloading
+    data:{ preload:true },//also need to add data object for custome preload and true the property preload and if preload:false then module will not preload 
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   }, 
 
@@ -28,7 +38,11 @@ export const ROUTES: Routes = [
     HttpClientModule,
     MailModule,
   
-    RouterModule.forRoot(ROUTES , { preloadingStrategy : PreloadAllModules } )//preload all our modules even it is lazyload //{ enableTracing: true } tracing is for debuging that where is the router is navigating show everythis in console
+    RouterModule.forRoot(ROUTES , { preloadingStrategy : CustomPreload } )//customPreload for our own choice which one module should be preload //preload all our modules even it is lazyload //{ enableTracing: true } tracing is for debuging that where is the router is navigating show everythis in console
+  ],
+  providers:[ 
+    //also need to provide custom class CustomPreload
+    CustomPreload
   ],
   bootstrap: [
     AppComponent
