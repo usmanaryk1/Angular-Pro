e3b179961470ea6092b10425b3fb99422a33fca6
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-09-16 10:57:10 +0500

    Providers and useClass

--- src/app/containers/pizza-viewer.component.ts
+++ src/app/containers/pizza-viewer.component.ts
@@ -12,7 +12,8 @@ interface Pizza {
   selector: 'pizza-viewer',
   //here we have useClass meand our own providers in the component usually its proveide in app.module.ts
   providers: [
-    FoodService
+    // FoodService, // this is a shorthand syntax of {provide:FoodService , useClass:FoodService} use one only if both name are same like
+    {provide:FoodService , useClass:FoodService}//for moke/fake api we use that useClass for testing purpose etc..
   ],
   template: `
     <div>
