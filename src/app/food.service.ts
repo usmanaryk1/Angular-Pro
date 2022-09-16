969a0ab125ccbc24d171887de757ed21c8053e83
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-09-16 10:16:22 +0500

    Providers and useValue so provide useValue in providers array Providers:[{provide:'api', useValue:'http://localhost:3000/pizzas'}] and inject this in our service with @inject('api') private api:string and then use this.api in service call

--- src/app/food.service.ts
+++ src/app/food.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { tap , catchError} from 'rxjs/operators';
 
@@ -8,13 +8,14 @@ import { HttpClient } from "@angular/common/http";
 @Injectable()
 export class FoodService {
 
-  //hard codded api its a initial/first step of learning 
-  api:string= "http://localhost:3000/pizzas"; // all component will show only pizzas
-  //after that we will use useValue in provider { provide: 'api', useValue: 'http://localhost:3000/pizzas' } so our whole application will consume this by name api which provide in app.module.ts in providers:[ ]  so there is 4 different types of providers like useValue we will se in next commits
-
+//use useValue in provider { provide: 'api', useValue: 'http://localhost:3000/pizzas' } so our whole application will consume this by name api which provide in app.module.ts in providers:[ ]  so there is 4 different types of providers total like one use here useValue we will see others 3 in next commits
+// inject useValue provide string type DI token in constructor to use it as DI
+// @Inject('api') private api: string
 
   constructor(
     private http:HttpClient,
+    // inject our api with @decarator
+    @Inject('api') private api: string, //useValue provide string type DI token here
   ) {}
 
   getFood(): Observable<any> {
