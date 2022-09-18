eb36fe1d4961e46290a2e5db7d3ad3591b10e51a
DESKTOP-SSEISDQ\Tech Usman Arshad <usmanaryk1@gmail.com>
2022-09-18 10:31:30 +0500

    Zones and NgZone so run outside of angular task and comes back result optionally means ngZone keep doing task outside of angular asynchronouslly such as settimeout, click, event handler or any other asynchronous operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
-import { FoodStoreService } from './food-store/food-store.service';
+import { Component, OnInit, DoCheck, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styles: [`
-    pizza-viewer,
-    side-viewer,
-    drink-viewer {
-      display: block;
-      border-bottom: 2px solid #eee;
-      padding: 20px 0;
-    }
-  `],
   template: `
-  <div>
-  Food Store ({{ (store | async)?.name }})
-</div>
+    <div>
+      Counter: {{ counter }}
+    </div>
   `
 })
-export class AppComponent {
-  store = this.foodService.getStore();
-  constructor(private foodService: FoodStoreService) {
-
+export class AppComponent implements OnInit, DoCheck {
+  counter = 0;
+  constructor(
+    private zone: NgZone //ngZone keep doing task outside of angular synchronouslly
+  ) {}
+  ngOnInit() {
+    this.zone.runOutsideAngular(() => {// there are many zones and runOutsideAngular is one of them
+      for (let i = 0; i < 100; i++) {
+        setTimeout(() => this.counter++);
+      }
+      this.zone.run(() => { //if not run then outside of angular it will keep runing and when we do this.zone.run then it comes back and give response/output/result
+        setTimeout(() => this.counter = this.counter, 1000);
+      });
+    });
+  }
+  ngDoCheck() {
+    console.log('Change detection has been run!'); // third time it will log when counter will come back with 100 and show result in template after 1 sec, can see clearly counter will be 0 for one second then it will update to 100 
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-//http module
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FoodStoreModule } from './food-store/food-store.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-
-    
-    //configurable ngModule
-    //before making forRoot it was just FoodStoreModule
-    //Means which particular store will use this module
-    FoodStoreModule.forRoot({storeId: 10292,
-        storeToken: 'eca938c99a0e9ff91029dc'
-      })
-   
+    BrowserModule
   ],
   bootstrap: [
     AppComponent
   ]
-
 })
 export class AppModule {}
